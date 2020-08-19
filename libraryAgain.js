f0b451ca7cdecbad8cdaf9a1df79d3fd9e198cfe
Rebecca Viner <rtviner@gmail.com>
2020-08-19 15:37:47 -0600

    remove methods from book objects and add to book proto, add updateIsRead method

--- libraryAgain.js
+++ libraryAgain.js
@@ -1,17 +1,22 @@
 let myLibrary = []; 
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-
-  this.info = function() {
+function bookCreator(title, author, pages, isRead) {
+  const newBook = Object.create(bookFunctions);
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  newBook.isRead = isRead;
+  return newBook;
+};
+const bookFunctions = {
+  getInfo: function () {
     let info = `You have ${this.isRead ? "read" : "not read"} ${this.title} by ${this.author}, ${this.pages} pages.`
     return info;
-  };
-};
-
+  },
+  updateIsRead: function () {
+    this.isRead = !this.isRead;
+  }
+}
 function addBookToLibrary(input) {
   let title = input[0].value;
   let author = input[1].value;
@@ -20,9 +25,9 @@ function addBookToLibrary(input) {
   //take users input
   // check if title, author and isRead data has been input and are the appropriate data types 
   // make a new book object with this data
-  let newBook = new Book(title, author, pages, isRead);
+  let currBook = bookCreator(title, author, pages, isRead);
     // push the new book to the myLibrary array
-  myLibrary.push(newBook);
+  myLibrary.push(currBook);
   render();
 };
 
@@ -48,6 +53,7 @@ function render() {
       // loop through myLibrary array and display each book on the page
       // each book is a separate div??
       myLibrary.map((book,index) => {
+
         let card = document.createElement('div');
         card.setAttribute('id', index);
         let title = document.createElement('h3');
@@ -66,10 +72,15 @@ function render() {
         readit.setAttribute('name', 'isRead');
         readit.setAttribute('id', 'isRead');
         if (book.isRead) readit.setAttribute('checked', true);
+        readit.addEventListener('change', () => {
+          let index = readit.parentElement.id;
+          myLibrary[index].updateIsRead();
+        })
         // need to make readit a checkbox and check if isRead is true;
         let readitLabel = document.createElement('label');
         readitLabel.setAttribute('for', 'isRead');
         readitLabel.textContent = "Read It";
+
 
         let deleteBtn = document.createElement('button');
         deleteBtn.textContent = "Remove";
